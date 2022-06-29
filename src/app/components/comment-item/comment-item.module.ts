2107f4a450153c40a9b6216a792cce7f614c31bf
Ricardo Tibola <ricardo.tibola@goimage.com.br>
2022-06-29 14:10:14 -0300

    [OK] Tecnical test UI B - first commit. Base structure

---
+++ src/app/components/comment-item/comment-item.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentItemComponent } from './comment-item.component';
+
+
+
+@NgModule({
+  declarations: [CommentItemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommentItemModule { }
---
+++ src/app/components/comments-list/comments-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentsListComponent } from './comments-list.component';
+
+
+
+@NgModule({
+  declarations: [CommentsListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommentsListModule { }
